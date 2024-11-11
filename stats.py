22932fc54aa7dd45916f43db4a50e62709e3fdb4
Adeiyo <jounaid141@gmail.com>
2024-11-11 17:27:56 +0100

    added input validation for score beatween 0 and 20

--- stats.py
+++ stats.py
@@ -1,12 +1,14 @@
 Python 3.12.3 (main, Sep 11 2024, 14:17:37) [GCC 13.2.0] on linux
 Type "help", "copyright", "credits" or "license()" for more information.
 >>> import numpy as np
-... 
-... def calculer_statistiques(notes):
-...     if len(notes) == 0:
-...         return None
-...     return {
-...         "moyenne": np.mean(notes),
-...         "médiane": np.median(notes),
-...         "écart_type": np.std(notes)
-...     }
+
+    def calculer_statistiques(notes):
+        notes = [note for note in notes if 0 <= note <= 20]
+        if len(notes) == 0:
+            return None
+        return {
+            "moyenne": np.mean(notes),
+            "médiane": np.median(notes),
+            "écart_type": np.std(notes)
+        }
+
